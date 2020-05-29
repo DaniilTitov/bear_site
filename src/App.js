6c3d4eaaeb3efc3d1a29e5161debc41c784293a5
DaniilTitov <daniil.a.titov@gmail.com>
2020-05-29 19:18:16 +0300

    added simple layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
 import './App.css';
+import Header from "./component/header/Header";
+import Footer from "./component/footer/Footer";
+import NotFound from "./component/not_found/NotFound";
+import Content from "./component/content/Content";
+import Users from "./component/users/Users";
 
 function App() {
     return (
-        <div className="App">
-            <p>
-                Здравствуйте! Как у вас делишки?
-            </p>
-        </div>
+        <Router>
+            <div className="App">
+
+                <div className="container">
+                    <Header/>
+
+                    <Switch>
+                        <Route exact path="/" component={Content}/>
+                        <Route path="/users" component={Users}/>
+                        {/*<Route path="/contact" component={Contact}/>*/}
+                        <Route component={NotFound}/>
+                    </Switch>
+                </div>
+
+                <Footer/>
+            </div>
+        </Router>
+
     );
 }
 
---
+++ src/component/not_found/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NotFound() {
+    return (
+        <div>
+            <p>
+                Тут ничего нет
+            </p>
+        </div>
+    )
+}
+
+export default NotFound;
